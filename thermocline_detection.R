36c72c27e1e37f730dafd01340e374c5e793f60c
Grace Wilkinson <grace.wilkinson23@gmail.com>
2022-10-13 20:32:31 -0500

    calculating the thermocline oocurence across lakes

---
+++ thermocline_detection.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+
+alm = read.csv("ALM Data 2000-2021 WIDE.csv") 
+
+#Thermocline 
+tcline = alm %>% 
+  filter(year>2007) %>% 
+  select(siteID, thermoclineDepth, year, doy) %>%
+  mutate(thermoclineDepth = case_when(is.na(.$thermoclineDepth) ~ 0, 
+                              TRUE ~ thermoclineDepth))
+
+tcline_sum = tcline %>%
+ count(thermoclineDepth>1, siteID) %>%
+  rename(tcline_logical = `thermoclineDepth > 1`) %>%
+  pivot_wider(id_cols = c(siteID), 
+              names_from = tcline_logical, 
+              values_from = n,
+              values_fill = 0) %>%
+  rename(no_tcline = `FALSE`,
+         yes_tcline = `TRUE`) %>%
+  mutate(n_tcline_obs = no_tcline + yes_tcline,
+         tcline_percent = (yes_tcline/n_tcline_obs)*100) %>%
+  filter(n_tcline_obs>9)
+
+plot(rank(tcline_sum$tcline_percent), tcline_sum$tcline_percent)
+hist(tcline_sum$tcline_percent)
